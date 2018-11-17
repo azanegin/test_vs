68a17e4d06aaba86bc67358498c4a1e64c141cd9
Alex Anon <gesserat@gmail.com>
2018-11-17 21:40:54 +0300

    123

---
+++ Project1/MyForm.h
@@ -0,0 +1,172 @@
+﻿#pragma once
+
+namespace Project1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		static int count = 0;
+	private: System::Windows::Forms::CheckBox^  checkBox1;
+	private: System::Windows::Forms::ProgressBar^  progressBar1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Button^  button1;
+	public:
+			 static int red = 255;
+			 static int slope = 0;
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^  chart1;
+	private: System::Windows::Forms::Timer^  timer1;
+	private: System::ComponentModel::IContainer^  components;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Series^  series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::DataPoint^  dataPoint1 = (gcnew System::Windows::Forms::DataVisualization::Charting::DataPoint(10,
+				1));
+			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->progressBar1 = (gcnew System::Windows::Forms::ProgressBar());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// chart1
+			// 
+			chartArea1->Name = L"ChartArea1";
+			this->chart1->ChartAreas->Add(chartArea1);
+			this->chart1->Location = System::Drawing::Point(12, 12);
+			this->chart1->Name = L"chart1";
+			series1->ChartArea = L"ChartArea1";
+			series1->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Spline;
+			series1->Name = L"Series1";
+			series1->Points->Add(dataPoint1);
+			this->chart1->Series->Add(series1);
+			this->chart1->Size = System::Drawing::Size(727, 329);
+			this->chart1->TabIndex = 0;
+			this->chart1->Text = L"chart1";
+			// 
+			// timer1
+			// 
+			this->timer1->Tick += gcnew System::EventHandler(this, &MyForm::timer1_Tick);
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(99, 372);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(179, 21);
+			this->checkBox1->TabIndex = 1;
+			this->checkBox1->Text = L"Отрицательные числа";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// progressBar1
+			// 
+			this->progressBar1->Location = System::Drawing::Point(80, 430);
+			this->progressBar1->Name = L"progressBar1";
+			this->progressBar1->Size = System::Drawing::Size(219, 23);
+			this->progressBar1->TabIndex = 2;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(80, 484);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(237, 22);
+			this->textBox1->TabIndex = 3;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(99, 549);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(167, 61);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"Построить!";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(768, 688);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->progressBar1);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->chart1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void MyForm_MouseHover(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void timer1_Tick(System::Object^  sender, System::EventArgs^  e) {
+		int sign = 1;
+		if (this->checkBox1->Checked) {
+			sign = -1;
+		}
+		System::Windows::Forms::DataVisualization::Charting::DataPoint^  dp = 
+			(gcnew System::Windows::Forms::DataVisualization::Charting::DataPoint(this->count, this->slope * this->count * sign));
+		this->chart1->Series[L"Series1"]->Points->Add(dp);
+		if (this->count < 255) {
+			this->count++;
+			this->progressBar1->Increment(1);
+		}
+		else {
+			this->timer1->Stop();
+		}
+	}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	System::String^ input = this->textBox1->Text;
+	this->slope = Convert::ToInt32(input);
+	this->timer1->Start();
+}
+};
+}
